1feaad82dab551076630ab3ad37169d89c078b8d
Dylan Juarez <dylanoscarjuarez2010@gmail.com>
2026-04-29 15:11:22 -0300

    Subo Nuevos Tps

--- Etapa2/17_MiPrimerMenu/17_MiPrimerMenu/Program.cs
+++ Etapa2/17_MiPrimerMenu/17_MiPrimerMenu/Program.cs
@@ -10,33 +10,31 @@ namespace _17_MiPrimerMenu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--- Boludeces que se me ocurrieron ---");
-            Console.WriteLine("1. Sumar 2 + 4");
-            Console.WriteLine("2. Restar 4 - 2");
-            Console.WriteLine("3. Decir 'Hola profe'");
-            Console.WriteLine("4. Salir");
+            Console.WriteLine("Bienvenido al menú: ");
+            Console.WriteLine("1. bienvenidos al himalaya");
+            Console.WriteLine("2. Aguante boca");
+            Console.WriteLine("3. 8 * 8");
+            Console.WriteLine("4. Salir del programa");
             int opcion = int.Parse(Console.ReadLine());
-
             while (opcion != 4)
             {
                 switch (opcion)
                 {
                     case 1:
-                        Console.WriteLine("La suma de 2 + 4 es igual a 6");
+                        Console.WriteLine("bienvenidos al himalaya");
                         break;
                     case 2:
-                        Console.WriteLine("La resta de 4 - 2 es igual a 2");
+                        Console.WriteLine("Aguante boca");
                         break;
                     case 3:
-                        Console.WriteLine("Hola profe");
+                        Console.WriteLine("La multiplicación de 8 * 8 = 64");
                         break;
                     default:
-                        Console.WriteLine("Opción inválida.");
+                        Console.WriteLine("Opción no válida");
                         break;
                 }
                 opcion = int.Parse(Console.ReadLine());
             }
-
         }
     }
 }
