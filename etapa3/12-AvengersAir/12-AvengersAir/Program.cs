4ee05ce73e5bda75fbf9387e1b14e6b0ed4929a3
Dylan Juarez <dylanoscarjuarez2010@Gmail.com>
2026-06-23 16:34:55 -0300

    subko nuevos tps

---
+++ etapa3/12-AvengersAir/12-AvengersAir/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_AvengersAir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            int TotalAsientos = 0;
+
+            while (opcion != 7)
+            {
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine(" Menú Principal - AvengersAir Vuelo Buenos Aires a Wakanda");
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Asientos disponibles: " + );
+                Console.WriteLine("Asientos Ocupados: " + );
+                Console.WriteLine("1 - Vender Asiento.");
+                Console.WriteLine("2 - Devolver Asiento.");
+                Console.WriteLine("3 - Modificar Asiento.");
+                Console.WriteLine("4 - Calcular Ventas.");
+                Console.WriteLine("5 - Buscar Pasajeros Por Edad.");
+                Console.WriteLine("6 - Obtener Asientos con DNI Par.");
+                Console.WriteLine("7 - Salir.");
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Ingrese la opción deseada: ");
+                switch (opcion)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        break;
+                    case 2:
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
